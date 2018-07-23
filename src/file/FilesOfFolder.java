625dedae795195229e25070275e3cce71ad501ae
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-07-23 00:08:30 -0300

    Adjusted files

--- src/file/FilesOfFolder.java
+++ src/file/FilesOfFolder.java
@@ -4,11 +4,11 @@ import java.io.File;
 
 public class FilesOfFolder {
 
-	public FilesOfFolder() {}
+  public FilesOfFolder() {}
 
-	public File[] getFilesOfFolder(String path) {
-		File folder = new File(path);
-		return folder.listFiles();
-	}
+  public File[] getFilesOfFolder(String path) {
+    File folder = new File(path);
+    return folder.listFiles();
+  }
 
 }
